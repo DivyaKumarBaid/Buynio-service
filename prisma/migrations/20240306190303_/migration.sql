a76cdfcffcac58ca0165283a73fefc25f9347d66
Divya Baid <divya.baid@Divya-Baid-Y7QQ3J9L2C.local>
2024-03-07 00:33:15 +0530

    test2

--- prisma/migrations/20240306190303_/migration.sql
+++ prisma/migrations/20240306190303_/migration.sql
@@ -1,3 +1,9 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "Category" AS ENUM ('CLOTH', 'ELECTRONICS');
+
 -- CreateTable
 CREATE TABLE "Unverified_Users" (
     "id" SERIAL NOT NULL,
@@ -20,10 +26,29 @@ CREATE TABLE "Users" (
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "hashRT" TEXT,
-    "role" TEXT NOT NULL DEFAULT 'user',
+    "role" "Role" NOT NULL DEFAULT 'USER',
     "signInMethod" TEXT NOT NULL DEFAULT 'local.com',
+    "onBoarded" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Hops" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "ownerId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "logo" TEXT,
+    "instagramAccount" TEXT NOT NULL,
+    "otherReachout" TEXT,
+    "descrtiption" TEXT NOT NULL DEFAULT '',
+    "motto" TEXT NOT NULL DEFAULT '',
+    "category" "Category" NOT NULL,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Hops_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -43,3 +68,6 @@ CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "PasswordOtp_user_mail_key" ON "PasswordOtp"("user_mail");
+
+-- AddForeignKey
+ALTER TABLE "Hops" ADD CONSTRAINT "Hops_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
