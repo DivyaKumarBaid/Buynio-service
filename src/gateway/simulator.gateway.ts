67622df7b1235f9b124120994afc4d211540526f
Divya Baid <divyakrbaid@gmail.com>
2024-06-28 02:19:37 +0530

    feat: added room in socket for json update

--- src/gateway/simulator.gateway.ts
+++ src/gateway/simulator.gateway.ts
@@ -1,20 +1,48 @@
 // src/gateways/simulator.gateway.ts
-import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody, ConnectedSocket } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from "@nestjs/websockets";
+import { Server, Socket } from "socket.io";
 
 @WebSocketGateway({ cors: true })
 export class SimulatorGateway {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('joinRoom')
-  handleJoinRoom(@MessageBody() room: string, @ConnectedSocket() client: Socket): void {
+  @SubscribeMessage("joinRoom")
+  handleJoinRoom(
+    @MessageBody() room: string,
+    @ConnectedSocket() client: Socket
+  ): void {
     client.join(room);
-    client.emit('joinedRoom', room);
+    client.emit("joinedRoom", room);
   }
 
-  @SubscribeMessage('messageToRoom')
-  handleMessage(@MessageBody() payload: { room: string; message: string }, @ConnectedSocket() client: Socket): void {
-    this.server.to(payload.room).emit('messageToClient', payload.message);
+  @SubscribeMessage("messageToRoom")
+  handleMessage(
+    @MessageBody() payload: { room: string; message: string }
+    // @ConnectedSocket() client: Socket
+  ): void {
+    this.server.to(payload.room).emit("messageToClient", payload.message);
+  }
+
+  @SubscribeMessage("elemSelectedToRoom")
+  handleElemSelected(
+    @MessageBody() payload: { room: string; message: string }
+    // @ConnectedSocket() client: Socket
+  ): void {
+    this.server.to(payload.room).emit("elemSelectedToClient", payload.message);
+  }
+
+  @SubscribeMessage("updateJsonToRoom")
+  handleUpdateJson(
+    @MessageBody() payload: { room: string; message: any }
+    // @ConnectedSocket() client: Socket
+  ): void {
+    this.server.to(payload.room).emit("updateJsonToClient", payload.message);
   }
 }
